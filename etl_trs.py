08e3b811cd3ce6cb24022bb2f21bd9c43998e342
HarisNaveed17 <haris.naveed174@gmail.com>
2022-04-20 12:31:36 +0500

    add separate ticker function

--- etl_trs.py
+++ etl_trs.py
@@ -3,6 +3,21 @@ from utils import *
 import json
 
 
+def extend_inferdata_text(df, infer_column, other_column):
+    if other_column is not None:
+        other_column.extend(infer_column)
+        df = df[other_column]
+
+    # df[infer_column] = df[infer_column].apply(lambda x: literal_eval(x)) turn this on if working with a csv on local machine
+    df['textInfer'] = df[infer_column].apply(lambda x: combine_infer_text(x))
+    df.drop(infer_column, axis=1, inplace=True)
+    for channel in pd.unique(df['channelName']):
+        chl_df = df[df['channelName'] == channel].reset_index(drop=True)
+        new_df = chl_df.explode('textInfer').reset_index(drop=True)
+        channel = channel + f'_trs'
+        yield new_df, channel
+
+
 def run_trs_pipeline(oltp_db, ann_db, dwh_db, time_filter=None, client='mongodb://localhost:27017'):
     with open('dbconfig.json') as conf_file:
         settings = json.load(conf_file)
@@ -14,8 +29,8 @@ def run_trs_pipeline(oltp_db, ann_db, dwh_db, time_filter=None, client='mongodb:
     delete_collection(connector, oltp_db, tgt_coll_oltp)
     # push_to_mongo(connector, trs_data, dwh_db, 'dwh_trs')
     trs_data.dropna(inplace=True)
-    ticker_words = extend_inferdata(
-        trs_data, infer_column='trsInfer', other_column=['timestamp', 'channelName'], mode='trs')
+    ticker_words = extend_inferdata_text(
+        trs_data, infer_column='output', other_column=['timestamp', 'channelName'])
     for ticker, chnl in ticker_words:
         push_to_mongo(connector, ticker, dwh_db, chnl)
 
