b08c362816daf64e1a103c96dbba9604d28a6990
HarisNaveed17 <haris.naveed174@gmail.com>
2022-04-20 12:29:59 +0500

    add separate face function

--- etl_frs.py
+++ etl_frs.py
@@ -3,6 +3,33 @@ from utils import *
 import json
 
 
+def extend_inferdata_face(df, infer_column_tag, other_column=None):
+    """extends labels of detected faces from multiple columns to multiple rows against the same frame and
+    timestamp (duplicates rows equal to number of labelled faces)
+
+    Args:
+        df (Pandas DataFrame): Input raw dataframe
+        infer_column_tag (string): a string label contained in the name of every face label column name (frsInfer.O.Label
+        in this case)
+        other_column (list): list of other columns that the user wants to include. Defaults to None
+
+    Yields:
+        tuple: a tuple of the processed dataframe and channel name
+    """
+    
+    lab_cols = [lab for lab in df.columns if infer_column_tag in lab]
+    if other_column is not None:
+        other_column.extend(lab_cols)
+        ndf = df[other_column]
+    ndf['combined'] = ndf[lab_cols].values.tolist()
+    ndf.drop(lab_cols, axis=1, inplace=True)
+    for channel in pd.unique(df['channelName']):
+        chl_df = df[df['channelName'] == channel].reset_index(drop=True)
+        new_df = chl_df.explode('combined').reset_index(drop=True)
+        channel = channel + f'_frs'
+        yield new_df, channel
+
+
 def run_frs_pipeline(oltp_db, ann_db, dwh_db, time_filter=None, client='mongodb://localhost:27017'):
     with open('dbconfig.json') as conf_file:
         settings = json.load(conf_file)
@@ -14,7 +41,7 @@ def run_frs_pipeline(oltp_db, ann_db, dwh_db, time_filter=None, client='mongodb:
     delete_collection(connector, oltp_db, tgt_coll_oltp, time_filter)
     # push_to_mongo(connector, frs_data, dwh_db, 'dwh_frs')
     frs_data.dropna()
-    people = extend_inferdata(
+    people = extend_inferdata_face(
         frs_data, infer_column='frsInfer', other_column=['timestamp', 'channelName'])
     for chnl_data, chnl in people:
         push_to_mongo(connector, chnl_data, dwh_db, chnl)
